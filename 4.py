8cb55094378df7ebc61b545df34ae4ea906f1142
olenashevchenko <olenasshevchenko@gmail.com>
2017-03-11 01:25:06 +0200

    Delete 4.py

--- 4.py
+++
@@ -1,9 +0,0 @@
-def is_year_lip(a):
-    if ((a%4==0) and (a%100!=0))or (a%400 == 0):
-        a="True"
-    else:
-        a="False"
-    return(a)
-a = input("year = ")
-a = int(a)
-print (is_year_lip(a))
